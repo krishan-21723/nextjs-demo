059098afc2a1453a21b19d7aea0fab0dbb8a1755
krishan21723 <kchhimpa@noon.com>
2021-10-28 00:09:00 +0530

    router root navigation aftet addtioin of meetup

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
+
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
+  const router = useRouter();
+
   async function addMeetupHandler(enteredMeetupData) {
     const response = await fetch("/api/new-meetup", {
       method: "POST",
